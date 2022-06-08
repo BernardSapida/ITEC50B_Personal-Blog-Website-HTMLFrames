50affec7b8c6497ae54db3e6e9ef989526a82b40
BernardSapida <bernardsapida1706@gmail.com>
2022-06-08 12:19:59 +0800

    git commit -m "first commit"

---
+++ javascript/navigation.js
@@ -0,0 +1,15 @@
+$(document).ready(function(){
+    $("#navigation").click(function(){
+        $("body").toggleClass("disableScroll");
+        $(".top").toggleClass("topAnimate");
+        $(".middle").toggleClass("middleAnimate");
+        $(".bottom").toggleClass("bottomAnimate");
+        $(".screen_navigation").toggleClass("show");
+        $("#navigation").css({"z-index":"2", "position":"sticky"});
+    });
+    
+    $(".links").click(function(){
+       $(".links").removeClass("active");
+       $(this).addClass("active");
+    });
+});
